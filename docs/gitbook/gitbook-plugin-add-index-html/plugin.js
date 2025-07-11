ed034bda933f4059793817e2293d105001603c16
luhuifang <luhuifang@genomics.cn>
2025-07-11 11:31:07 +0800

    update:20250711113107

---
+++ docs/gitbook/gitbook-plugin-add-index-html/plugin.js
@@ -0,0 +1,31 @@
+require(['gitbook', 'jquery'], function(gitbook, $) {
+    var opts;
+
+    gitbook.events.bind('start', function(e, config) {
+        opts = config['add-index-html'].elements;
+    });
+
+    gitbook.events.bind('page.change', function() {
+        $.map(opts, function(ele) {
+            $(ele).each((index, value) => {
+                const attr = value.attributes.getNamedItem('href');
+                if(attr){
+                    const href = attr.value.toLowerCase()
+                    // console.log(href)
+                    
+                    if( href.match(/^(?!.*png)(?!.*html)(?!.*\#)(?!.*http).*$/)){
+                        attr.value += href.match(/\/$/) ? 'index.html' : '/index.html'
+                    } else if(href.match(/^(?!#).*$/) && href.match(/^.*\#.*$/) && href.match(/^(?!.*html\#).*$/)){
+                        console.log('*******222', href)
+                        // 优化判断：如果包含"/#"，则替换"/#"为"/index.html#"，否则替换第一个"#"为"/index.html#"
+                        if(href.indexOf('/#') !== -1){
+                            attr.value = href.replace('#', 'index.html#');
+                        } else {
+                            attr.value = href.replace('#', '/index.html#');
+                        }
+                    }
+                }
+            });
+        });
+    });
+});
